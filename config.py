cccc83d47456bfacd7bd366343f6dd6f9728acef
thirdeyeai2 <rifas786786786@gmail.com>
2026-04-06 20:43:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
+# config.py
+BOT_TOKEN = "8040350855:AAEaKZj6pRY5_8Weajb0jo6MWyWNEtOFQdk"
+API_ID = 35632117        # your Telegram API ID
+API_HASH = "5c619323bc6b20b0c120886ae1316e27"
 
-import json
-import os
-
-with open("data.json") as f:
-    d = json.load(f)
-
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-GROUPS = d["groups"]
+# List of group IDs where VC monitoring is active
+GROUPS = [-1001747095956]
