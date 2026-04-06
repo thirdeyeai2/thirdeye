1bfc4cd0b61ba61975d30ceb33f1ffa865310c18
thirdeyeai2 <rifas786786786@gmail.com>
2026-04-06 16:00:12 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import json
+import os
+
+with open("data.json") as f:
+    d = json.load(f)
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+GROUPS = d["groups"]
