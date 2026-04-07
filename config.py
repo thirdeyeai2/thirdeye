829a06f7450c68b7321b14b817277058a6185811
thirdeyeai2 <rifas786786786@gmail.com>
2026-04-07 11:00:35 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-BOT_TOKEN = "8040350855:AAEaKZj6pRY5_8Weajb0jo6MWyWNEtOFQdk"
-API_ID = 35632117        # your Telegram API ID
-API_HASH = "5c619323bc6b20b0c120886ae1316e27"
-
-# List of group IDs where VC monitoring is active
-GROUPS = [-1001747095956]
