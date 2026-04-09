0e07f6d7641f98fecf56fb03a7682e1fc469b4dc
thirdeyeai2 <rifas786786786@gmail.com>
2026-04-10 01:58:24 +0530

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -11,7 +11,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 SESSION_STRING = os.getenv("SESSION_STRING")
 
-app = Client("userbot", api_id=API_ID, api_hash=API_HASH, session_string=STRING)
+app = Client("userbot", api_id=API_ID, api_hash=API_HASH, session_string=SESSION_STRING)
 
 # Load config.json
 def load():
