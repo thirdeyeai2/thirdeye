155bd29148b6087053192a70ca4b8dc278022f4e
thirdeyeai2 <rifas786786786@gmail.com>
2026-04-16 03:07:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 SESSION_STRING = os.getenv("SESSION_STRING")
 
 GROUP_ID = int(os.getenv("GROUP_ID"))
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
 
 PORT = int(os.getenv("PORT", 8000))
